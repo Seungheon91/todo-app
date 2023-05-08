6040ce5019d89bc736dde0bf43dda037045dcf43
StarkLSH <seungheon1029@gmail.com>
2023-05-08 10:59:41 +0900

    list, listItem style 추가

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -3,9 +3,16 @@ import './TodoListItem.scss'
 
 const TodoListItem = () => {
   return (
-    <div className=''>
-      <div></div>
-      
+    <div className="TodoListItem">
+      <div className="checkbox">
+        <MdCheckBoxOutlineBlank/>
+        <div className="text">할 일</div>
+      </div>
+      <div className="remove">
+        <MdRemoveCircleOutline/>
+      </div>
     </div>
   )
 }
+
+export default TodoListItem
